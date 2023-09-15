817a8516e2472b7e715595e84322687747425593
Janhvi Kalariya <100296727+Janhvi-Kalariya@users.noreply.github.com>
2023-09-15 17:42:31 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,36 @@
+function rollDice() {
+
+	var randomNumber1 = Math.floor(Math.random() * 6) + 1; //1-6
+
+	var randomDiceImage = "dice" +randomNumber1+ ".png";//dice1.png - dice6.png
+
+	var randomImageSource = "images/" +randomDiceImage;//images/dice1.png - images/dice6.png 
+
+	var image1 = document.querySelectorAll("img")[0];
+
+	image1.setAttribute("src", randomImageSource);
+
+
+	var randomNumber2 = Math.floor(Math.random() * 6) + 1; //1-6
+
+	var randomImageSource2 = "images/dice" +randomNumber2+ ".png";
+
+	document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
+
+	//winner,losser or draw
+	if (randomNumber1 > randomNumber2){
+    	document.querySelector("h1").textContent = "Player 1 Wins! 🚩";
+	}
+	else if(randomNumber2 > randomNumber1){
+    	document.querySelector("h1").textContent = "Player 2 Wins! 🚩";
+	}
+	else{
+    	document.querySelector("h1").textContent = "Draw! 🏳️";
+	}
+
+}
+
+// Add an event listener to the button to trigger the dice roll when clicked
+var rollButton = document.getElementById("rollButton");
+rollButton.addEventListener("click", rollDice);
+
